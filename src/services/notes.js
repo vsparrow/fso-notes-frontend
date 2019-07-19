ad68f087d24b259aabf2a7eed3d01c8f9d83a60a
khan@vsparrow <khan@vsparrow.com>
2019-07-19 01:12:32 +0000

    update baseUrl

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // const baseUrl = 'http://fullstackopen-vsparrow.c9users.io:8081/notes';
 // const baseUrl = 'https://infinite-basin-47509.herokuapp.com/notes/';
-const baseUrl = '/notes'; //this is when we * build * the react app and place it in the backend's folder
+const baseUrl = '/api/notes'; //this is when we * build * the react app and place it in the backend's folder
 
 const getAll = () => axios.get(baseUrl);
 
